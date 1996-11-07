c305e650b44232cf3ec7a52d94f0f01aaf838299
martin <martin@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1996-11-07 15:42:51 +0000

    PLT_DEBUG hinzugefuegt
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@187 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/plt/include/plt/debug.h
+++ history/cvs-legacy/acplt/trunk/plt/include/plt/debug.h
@@ -17,12 +17,18 @@
 #include <plt/config.h>
 
 #ifdef NDEBUG
+#define PLT_DEBUG 0
+#else
+#define PLT_DEBUG 1
+#endif
+
+#if PLT_DEBUG
 
 #define PLT_DEBUG_PRECONDITIONS 0
 #define PLT_DEBUG_POSTCONDITIONS 0
 #define PLT_DEBUG_INVARIANTS 0
 
-#else /* NDEBUG */
+#else /* PLT_DEBUG */
 
 #ifndef PLT_DEBUG_PRECONDITIONS
 #define PLT_DEBUG_PRECONDITIONS 1
@@ -36,7 +42,7 @@
 #define PLT_DEBUG_INVARIANTS 1
 #endif
 
-#endif /* NDEBUG */
+#endif /* PLT_DEBUG */
 
 #if PLT_DEBUG_PRECONDITIONS
 #define PLT_PRECONDITION(x) assert(x)
