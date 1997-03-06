0853bc5e1cb22d58b396ee3828283a8f8c7049bf
martin <martin@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1997-03-06 12:14:25 +0000

    *** empty log message ***
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@260 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/plt/include/plt/handle.h
+++ history/cvs-legacy/acplt/trunk/plt/include/plt/handle.h
@@ -1,6 +1,7 @@
 /* -*-plt-c++-*- */
 #ifndef PLT_HANDLE_INCLUDED
 #define PLT_HANDLE_INCLUDED
+/* $Header: /home/david/cvs/acplt/plt/include/plt/handle.h,v 1.6 1997-03-06 12:14:25 martin Exp $ */
 /*
  * Copyright (c) 1996, 1997
  * Chair of Process Control Engineering,
