f3d949395db1c124225acfc858efbf037a6850f4
markusj <markusj@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1997-03-12 16:03:05 +0000

    parameters and results for services defined in KsServerBase
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@294 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

---
+++ history/cvs-legacy/acplt/trunk/ks/include/ks/serviceparams.h
@@ -0,0 +1,343 @@
+/* -*-plt-c++-*- */
+#ifndef KS_SERVICEPARAMS_INCLUDED
+#define KS_SERVICEPARAMS_INCLUDED
+/*
+ * Copyright (c) 1996, 1997
+ * Chair of Process Control Engineering,
+ * Aachen University of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must print or display the above
+ *    copyright notice either during startup or must have a means for
+ *    the user to view the copyright notice.
+ * 3. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the Chair of Process Control Engineering nor the
+ *    name of the Aachen University of Technology may be used to endorse or
+ *    promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE CHAIR OF PROCESS CONTROL ENGINEERING
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE CHAIR OF PROCESS CONTROL
+ * ENGINEERING BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Author : Markus Juergens <plt@rwth-aachen.de> */
+
+//////////////////////////////////////////////////////////////////////
+
+#include <ks/xdr.h>
+#include <ks/register.h>
+#include <ks/props.h>
+#include <ks/list.h>
+#include <ks/array.h>
+
+//////////////////////////////////////////////////////////////////////
+// Classes for GetPP service
+//   - class KsGetPPParams
+//   - class KsGetPPResult
+//////////////////////////////////////////////////////////////////////
+
+class KsGetPPParams 
+: public KsXdrAble
+{
+public:
+    KsGetPPParams(XDR *, bool &);
+    KsGetPPParams();
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsGetPPParams *xdrNew(XDR *);
+
+    KsString path;
+    KS_OBJ_TYPE type_mask;
+    KsString name_mask;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class KsGetPPResult
+: public KsResult
+{
+public:
+    KsGetPPResult();
+    KsGetPPResult(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsGetPPResult *xdrNew(XDR *);
+
+    KsList<KsProjPropsHandle> items;
+};
+
+//////////////////////////////////////////////////////////////////////
+// Classes for GetVar service
+//   - class KsGetVarParams
+//   - class KsGetVarItemResult
+//   - class KsGetVarResult
+//////////////////////////////////////////////////////////////////////
+
+class KsGetVarParams
+: public KsXdrAble
+{
+public:
+    KsGetVarParams(size_t num_ids);
+    KsGetVarParams(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsGetVarParams *xdrNew(XDR *);
+
+    KsArray<KsString> identifiers;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class KsGetVarItemResult
+: public KsResult
+{
+public:
+    KsGetVarItemResult();
+    KsGetVarItemResult(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsGetVarItemResult *xdrNew(XDR *);
+
+    KsCurrPropsHandle item;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class KsGetVarResult 
+: public KsResult
+{
+public:
+    KsGetVarResult(size_t);
+    KsGetVarResult(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsGetVarResult *xdrNew(XDR *);
+
+    KsArray<KsGetVarItemResult> items;
+};
+
+
+//////////////////////////////////////////////////////////////////////
+// Classes for SetVar Service
+//   - class KsSetVarItem
+//   - class KsSetVarParams
+//   - class KsSetVarResult
+//////////////////////////////////////////////////////////////////////
+
+class KsSetVarItem
+: public KsXdrAble
+{
+public:
+    KsSetVarItem();
+    KsSetVarItem(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsSetVarItem *xdrNew(XDR *);
+
+    KsString path_and_name;
+    KsCurrPropsHandle curr_props;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class KsSetVarParams
+: public KsXdrAble
+{
+public:
+    KsSetVarParams(size_t);
+    KsSetVarParams(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsSetVarParams *xdrNew(XDR *);
+
+    KsArray<KsSetVarItem> items;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class KsSetVarResult
+: public KsResult
+{
+public:
+    KsSetVarResult(size_t);
+    KsSetVarResult(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsSetVarResult *xdrNew(XDR *);
+
+    KsArray<KsResult> results;
+};
+
+//////////////////////////////////////////////////////////////////////
+// Classes for ExgData Service
+//   - class KsSetVarItem(see SetVar service beyond)
+//   - class KsGetVarItemResult(see GetVar service beyond)
+//   - class KsExgDataParams
+//   - class KsExgDataResult
+//////////////////////////////////////////////////////////////////////
+
+class KsExgDataParams 
+: public KsXdrAble
+{
+public:
+    KsExgDataParams(size_t set_num_items, size_t get_num_items);
+    KsExgDataParams(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsExgDataParams *xdrNew(XDR *);
+
+    KsArray<KsSetVarItem> set_vars;
+    KsArray<KsString> get_vars;
+};
+
+//////////////////////////////////////////////////////////////////////
+
+class KsExgDataResult
+: public KsResult
+{
+public:
+    KsExgDataResult(size_t set_num_items, size_t get_num_items);
+    KsExgDataResult(XDR *, bool &);
+
+    bool xdrEncode(XDR *) const;
+    bool xdrDecode(XDR *);
+    static KsExgDataResult *xdrNew(XDR *);
+
+    KsArray<KsResult> results;
+    KsArray<KsGetVarItemResult> items;
+};
+    
+//////////////////////////////////////////////////////////////////////
+// Inline Implementation
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsGetPPParams::KsGetPPParams()
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsGetPPResult::KsGetPPResult()
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsGetVarParams::KsGetVarParams(size_t num_ids)
+: identifiers(num_ids)
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline bool
+KsGetVarParams::xdrEncode(XDR *xdr) const 
+{
+    return identifiers.xdrEncode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+inline bool
+KsGetVarParams::xdrDecode(XDR *xdr)
+{
+    return identifiers.xdrDecode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsGetVarItemResult::KsGetVarItemResult()
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsGetVarResult::KsGetVarResult(size_t nids)
+: items(nids)
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsSetVarItem::KsSetVarItem()
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline 
+KsSetVarParams::KsSetVarParams(size_t nitems)
+: items(nitems)
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline bool
+KsSetVarParams::xdrEncode(XDR *xdr) const
+{
+    return items.xdrEncode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+inline bool
+KsSetVarParams::xdrDecode(XDR *xdr)
+{
+    return items.xdrDecode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsSetVarResult::KsSetVarResult(size_t size)
+: results(size)
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsExgDataParams::KsExgDataParams(size_t nset, size_t nget)
+: set_vars(nset), get_vars(nget)
+{}
+
+//////////////////////////////////////////////////////////////////////
+
+inline
+KsExgDataResult::KsExgDataResult(size_t nset, size_t nget)
+: results(nset), items(nget)
+{}
+
+#endif
+
+
+
+
+
+
+
+
---
+++ history/cvs-legacy/acplt/trunk/ks/src/serviceparams.cpp
@@ -0,0 +1,318 @@
+/* -*-plt-c++-*- */
+
+/*
+ * Copyright (c) 1996, 1997
+ * Chair of Process Control Engineering,
+ * Aachen University of Technology.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must print or display the above
+ *    copyright notice either during startup or must have a means for
+ *    the user to view the copyright notice.
+ * 3. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 4. Neither the name of the Chair of Process Control Engineering nor the
+ *    name of the Aachen University of Technology may be used to endorse or
+ *    promote products derived from this software without specific prior
+ *    written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE CHAIR OF PROCESS CONTROL ENGINEERING
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE CHAIR OF PROCESS CONTROL
+ * ENGINEERING BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* Author : Markus Juergens <plt@rwth-aachen.de> */
+
+//////////////////////////////////////////////////////////////////////
+
+#include <ks/serviceparams.h>
+
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRNEW(KsGetPPParams);
+KS_IMPL_XDRCTOR(KsGetPPParams);
+
+//////////////////////////////////////////////////////////////////////
+
+bool 
+KsGetPPParams::xdrEncode(XDR *xdr) const
+{
+    return path.xdrEncode(xdr)
+        && xdr_enum(xdr, &type_mask)
+        && name_mask.xdrEncode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool 
+KsGetPPParams::xdrDecode(XDR *xdr) 
+{
+    return path.xdrDecode(xdr)
+        && xdr_enum(xdr, &type_mask)
+        && name_mask.xdrDecode(xdr);
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// class KsGetPPResult
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRNEW(KsGetPPResult);
+KS_IMPL_XDRCTOR(KsGetPPResult);
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsGetPPResult::xdrEncode(XDR *xdr) const
+{
+    // serialize members of base class
+    //
+    if( !KsResult::xdrEncode(xdr) ) return false;
+
+    // check for valid information and encode list contents
+    //
+    if( result != KS_ERR_OK ) {
+        return items.xdrEncode(xdr);
+    }
+    
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsGetPPResult::xdrDecode(XDR *xdr)
+{
+    // deserialize members of base class
+    //
+    if( !KsResult::xdrDecode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        // decode additional information
+        //
+        return items.xdrDecode(xdr);
+    }
+
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+// class KsGetVarParams
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRCTOR(KsGetVarParams);
+KS_IMPL_XDRNEW(KsGetVarParams);
+
+
+//////////////////////////////////////////////////////////////////////
+// class KsGetVarItemResult
+//////////////////////////////////////////////////////////////////////
+
+bool 
+KsGetVarItemResult::xdrEncode(XDR *xdr) const
+{
+    if( !KsResult::xdrEncode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return item.xdrEncode(xdr);
+    }
+
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsGetVarItemResult::xdrDecode(XDR *xdr)
+{
+    if( !KsResult::xdrDecode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return item.xdrDecode(xdr);
+    }
+
+    return true;
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// class KsGetVarResult
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRNEW(KsGetVarResult);
+KS_IMPL_XDRCTOR(KsGetVarResult);
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsGetVarResult::xdrEncode(XDR *xdr) const
+{
+    if( !KsResult::xdrEncode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return items.xdrEncode(xdr);
+    }
+    
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsGetVarResult::xdrDecode(XDR *xdr)
+{
+    if( !KsResult::xdrDecode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return items.xdrDecode(xdr);
+    }
+    
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+// classes for SetVar service
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRCTOR(KsSetVarItem);
+KS_IMPL_XDRNEW(KsSetVarItem);
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsSetVarItem::xdrEncode(XDR *xdr) const
+{
+    return path_and_name.xdrEncode(xdr)
+        && curr_props.xdrEncode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsSetVarItem::xdrDecode(XDR *xdr)
+{
+    return path_and_name.xdrDecode(xdr)
+        && curr_props.xdrDecode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRCTOR(KsSetVarParams);
+KS_IMPL_XDRNEW(KsSetVarParams);
+
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRCTOR(KsSetVarResult);
+KS_IMPL_XDRNEW(KsSetVarResult);
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsSetVarResult::xdrEncode(XDR *xdr) const
+{
+    if( !KsResult::xdrEncode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return results.xdrEncode(xdr);
+    }
+
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsSetVarResult::xdrDecode(XDR *xdr)
+{
+    if( !KsResult::xdrDecode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return results.xdrDecode(xdr);
+    }
+
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+// Classes for ExgData Service
+//   - class KsSetVarItem(see SetVar service beyond)
+//   - class KsGetVarItemResult(see GetVar service beyond)
+//   - class KsExgDataParams
+//   - class KsExgDataResult
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRCTOR(KsExgDataParams);
+KS_IMPL_XDRNEW(KsExgDataParams);
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsExgDataParams::xdrEncode(XDR *xdr) const
+{
+    return set_vars.xdrEncode(xdr)
+        && get_vars.xdrEncode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsExgDataParams::xdrDecode(XDR *xdr)
+{
+    return set_vars.xdrDecode(xdr)
+        && get_vars.xdrDecode(xdr);
+}
+
+//////////////////////////////////////////////////////////////////////
+
+KS_IMPL_XDRCTOR(KsExgDataResult);
+KS_IMPL_XDRNEW(KsExgDataResult);
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsExgDataResult::xdrEncode(XDR *xdr) const 
+{
+    if( !KsResult::xdrEncode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return results.xdrEncode(xdr)
+            && items.xdrEncode(xdr);
+    }
+
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+bool
+KsExgDataResult::xdrDecode(XDR *xdr)
+{
+    if( !KsResult::xdrDecode(xdr) ) return false;
+
+    if( result == KS_ERR_OK ) {
+        return results.xdrDecode(xdr)
+            && items.xdrDecode(xdr);
+    }
+
+    return true;
+}
+
+//////////////////////////////////////////////////////////////////////
+// End of File serviceparams.cpp
+//////////////////////////////////////////////////////////////////////
+
+    
