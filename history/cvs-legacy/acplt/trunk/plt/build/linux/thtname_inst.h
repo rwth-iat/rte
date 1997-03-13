510a760dc36cca9bf7749054b36f4aa5985c3560
martin <martin@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1997-03-13 09:06:14 +0000

    *** empty log message ***
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@304 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

---
+++ history/cvs-legacy/acplt/trunk/plt/build/linux/thtname_inst.h
@@ -0,0 +1,7 @@
+template class PltAssoc<PltString, Adresse *>;
+template class PltContainer<PltAssoc<PltString, Adresse *> >;
+template class PltDictionary<PltString, Adresse *>;
+template class PltHashIterator<PltString, Adresse *>;
+template class PltHashTable<PltString, Adresse *>;
+template class PltHashTable_<PltString, Adresse *>;
+template class PltIterator<PltAssoc<PltString, Adresse *> >;
