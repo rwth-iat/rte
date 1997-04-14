20de3bfac0caa4d1c3ff671180d3a33626b4de38
harald <harald@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1997-04-14 16:29:11 +0000

    Tests in eigenen CVS-Baum acplt_test ausgelagert.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@534 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/plt/build/hpux/thandle_inst.h
+++
@@ -1,5 +0,0 @@
-template class PltArrayHandle<Test>;
-template class PltHandle<Test>;
-template class PltPtrHandle<Test>;
-template class Plt_AtArrayNew<Test>;
-template class Plt_AtNew<Test>;
--- history/cvs-legacy/acplt/trunk/plt/build/hpux/tpriorityqueue_inst.h
+++
@@ -1,9 +0,0 @@
-template class PltContainer<PltPtrComparable<float> >;
-template class PltContainer<float>;
-template class PltIterator<PltPtrComparable<float> >;
-template class PltIterator<float>;
-template class PltPQIterator<PltPtrComparable<float> >;
-template class PltPQIterator<float>;
-template class PltPriorityQueue<PltPtrComparable<float> >;
-template class PltPriorityQueue<float>;
-template class PltPtrComparable<float>;
