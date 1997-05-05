8a7ba278a376c602d6a5c28b4d69b4a245383f01
harald <harald@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1997-05-05 06:45:49 +0000

    Regeln fuer den NT KS Manager Service mit aufgenommen.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@572 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/ks/build/nt/borland.mk
+++ history/cvs-legacy/acplt/trunk/ks/build/nt/borland.mk
@@ -1,5 +1,5 @@
 ### -*-makefile-*-
-### 
+###
 ### Compiler & platform specific part for
 ### Borland C++ / make / Windows NT
 
@@ -19,12 +19,17 @@ EXAMPLESSRCDIR = $(KSDIR)\examples\\
 ### Compiler
 CXX = bcc32
 CXX_FLAGS = -DNDEBUG -O2 -w
-#CXX_FLAGS = 
+#CXX_FLAGS = -DNDEBUG -w -v
+#CXX_FLAGS =
 CXX_EXTRA_FLAGS = -a8 -I. -I$(PLTDIR)\include -I$(KSDIR)\include -I$(ONCDIR) -DPLT_SYSTEM_NT=1
 
-.SUFFIXES:	
+RC = brc32
 
-.SUFFIXES:	.cpp .obj .lib .exe
+LIBKS_NT_OBJECTS = ntservice$(O)
+
+.SUFFIXES:
+
+.SUFFIXES:      .cpp .obj .lib .exe
 
 all: $(LIBKS)
 
@@ -40,28 +45,34 @@ all: $(LIBKS)
 		-Jgx $(CXX_EXTRA_FLAGS) $(CXX_FLAGS) -c -o$@ $<
 !
 
-templates.obj:	$(SRCDIR)templates.cpp
+templates.obj:  $(SRCDIR)templates.cpp
 	@echo Compiling $<
 	@$(CXX) @&&!
 		-Jgd $(CXX_EXTRA_FLAGS) $(CXX_FLAGS) -c -o$@ $(SRCDIR)templates.cpp
 !
 
-tmanager.obj:	$(EXAMPLESSRCDIR)tmanager.cpp
+tmanager.obj:   $(EXAMPLESSRCDIR)tmanager.cpp
 	@echo Compiling $<
 	@$(CXX) @&&!
 		-Jgd $(CXX_EXTRA_FLAGS) $(CXX_FLAGS) -c -o$@ $(EXAMPLESSRCDIR)tmanager.cpp
 !
 
-tserver.obj:	$(EXAMPLESSRCDIR)tserver.cpp
+tserver.obj:    $(EXAMPLESSRCDIR)tserver.cpp
 	@echo Compiling $<
 	@$(CXX) @&&!
 		-Jgd $(CXX_EXTRA_FLAGS) $(CXX_FLAGS) -c -o$@ $(EXAMPLESSRCDIR)tserver.cpp
 !
 
-tsclient.obj:	$(EXAMPLESSRCDIR)tsclient.cpp
+tsclient.obj:   $(EXAMPLESSRCDIR)tsclient.cpp
 	@echo Compiling $<
 	@$(CXX) @&&!
 		-Jgd $(CXX_EXTRA_FLAGS) $(CXX_FLAGS) -c -o$@ $(EXAMPLESSRCDIR)tsclient.cpp
+!
+
+ntksmanager_templates.obj:      $(EXAMPLESSRCDIR)ntksmanager_templates.cpp
+	@echo Compiling $<
+	@$(CXX) @&&!
+		-Jgd $(CXX_EXTRA_FLAGS) $(CXX_FLAGS) -c -o$@ $(EXAMPLESSRCDIR)ntksmanager_templates.cpp
 !
 
 
@@ -80,7 +91,7 @@ tsclient.obj:	$(EXAMPLESSRCDIR)tsclient.cpp
 		$< $(LIBKS) $(LIBPLT) $(LIBRPC)
 !
 
-examples:	tmanager.exe tserver.exe tsclient.exe
+examples:       ntksmanager.exe tmanager.exe tserver.exe tsclient.exe
 
 tmanager.exe: tmanager.obj tmanager1.obj $(LIBKS)
 	@echo Linking $@
@@ -100,11 +111,29 @@ tsclient.exe: tsclient.obj tsclient1.obj $(LIBKS)
 		tsclient.obj tsclient1.obj $(LIBKS) $(LIBPLT) $(LIBRPC)
 !
 
-$(LIBKS) : $(LIBKS_OBJECTS)
+ntksmanager.res: $(EXAMPLESSRCDIR)ntksmanager.rc
+	$(RC) -r -fontksmanager.res $(EXAMPLESSRCDIR)ntksmanager.rc 
+
+ntksmanager.exe: ntksmanager.obj ntksmanager_templates.obj $(LIBKS) ntksmanager.res
+	@echo Linking $@
+	$(CXX) @&&!
+		-v -tWM ntksmanager.obj ntksmanager_templates.obj $(LIBKS) $(LIBPLT) $(LIBRPC)
+!
+	$(RC) ntksmanager.res ntksmanager.exe
+
+### explicit dependencies not covered by platform-dependend depent mechanism
+ntservice$(O): $(SRCDIR)ntservice.cpp $(KSDIR)\include\ks\ntservice.h
+	@echo Compiling ntservice.cpp
+	@$(CXX) @&&!
+		-Jgx $(CXX_EXTRA_FLAGS) $(CXX_FLAGS) -c -o$@ $(SRCDIR)ntservice.cpp
+!
+
+$(LIBKS) : $(LIBKS_OBJECTS) $(LIBKS_NT_OBJECTS)
 	$(PLTDIR)\build\nt\plt_ar tlib $@ $(LIBKS_OBJECTS1)
 	$(PLTDIR)\build\nt\plt_ar tlib $@ $(LIBKS_OBJECTS2)
 	$(PLTDIR)\build\nt\plt_ar tlib $@ $(LIBKS_OBJECTS3)
 	$(PLTDIR)\build\nt\plt_ar tlib $@ $(LIBKS_OBJECTS4)
+	$(PLTDIR)\build\nt\plt_ar tlib $@ $(LIBKS_NT_OBJECTS)
 
 clean :
 	del *.obj
