5c056685e03f1766eb3578b05918a84d7bfd6857
harald <harald@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1998-10-06 13:19:16 +0000

    Anpassungen, so dass diese Headerdatei sowohl fuer C++ als auch fuer C benutzt werden kann, in Zusammenhang mit rpcgen.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@989 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/ks/include/ks/ks.h
+++ history/cvs-legacy/acplt/trunk/ks/include/ks/ks.h
@@ -1,5 +1,5 @@
 /* -*-plt-c++-*- */
-/* $Header: /home/david/cvs/acplt/ks/include/ks/ks.h,v 1.17 1998-10-01 09:59:10 markusj Exp $ */
+/* $Header: /home/david/cvs/acplt/ks/include/ks/ks.h,v 1.18 1998-10-06 13:19:16 harald Exp $ */
 #ifndef KS_KS_INCLUDED
 #define KS_KS_INCLUDED
 /*
@@ -37,34 +37,50 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "ks/rpc.h"
 
-/*
- * Version string for use with banners, messages, etc.
+/* ----------------------------------------------------------------------------
+ * In case this code is included from the C++ communication library code, then
+ * use the rpc header file provided by the C++ wrapper, because that particular
+ * header file encapsulates different RPC header mumbo jumbo especially on
+ * OpenVMS. When not using C++, then include the ordinary rpc headers.
+ */
+#ifdef __cplusplus
+#include "ks/rpc.h"
+#else
+#include <rpc/rpc.h>
+#include <rpc/xdr.h>
+#endif
+
+
+/* ----------------------------------------------------------------------------
+ * Version string for use with banners, messages, etc. This is used for example
+ * in the managers as well as in the test servers for the /vendor/version
+ * communication object.
  */
 #define KS_VERSION_STRING "1.0.5pre1"
 	 
-/*
+/* ----------------------------------------------------------------------------
  * RPC program number reserved for ACPLT/KS library
  */
 const unsigned long KS_RPC_PROGRAM_NUMBER = 0x49678; 
 
-/*
- * default protocol version
+/* ----------------------------------------------------------------------------
+ * Default protocol version.
  */ 
 const unsigned long KS_PROTOCOL_VERSION = 1;
 
-/*
- * Currently defined ACPLT/KS error codes.
+
+/* ----------------------------------------------------------------------------
+ * ACPLT/KS error codes defined for the core protocol...
  */
 enum KS_RESULT_ENUM {
     KS_ERR_OK                       = 0x0000,
     KS_ERR_GENERIC                  = 0x0001,
+    KS_ERR_TARGETGENERIC            = 0x0006,
     KS_ERR_BADAUTH                  = 0x0002,
     KS_ERR_UNKNOWNAUTH              = 0x0005,
     KS_ERR_NOTIMPLEMENTED           = 0x0003,
     KS_ERR_BADPARAM                 = 0x0004,
-    KS_ERR_TARGETGENERIC            = 0x0006,
    
     KS_ERR_BADNAME                  = 0x0010,
     KS_ERR_BADPATH                  = 0x0011,
@@ -73,12 +89,15 @@ enum KS_RESULT_ENUM {
     KS_ERR_BADTYPE                  = 0x0014,
     KS_ERR_CANTSYNC                 = 0x0015,
     KS_ERR_BADSELECTOR              = 0x0016,
+    KS_ERR_BADINITPARAM             = 0x0017,
+    KS_ERR_NOFACTORY                = 0x0018,
     
     KS_ERR_NOREMOTE                 = 0x0020,
     KS_ERR_SERVERUNKNOWN            = 0x0021,
 
 /*  
- *  errors occurring in the client part
+ *  ... and errors which can occur in the client part of the
+ *  C++ Communication Library.
  */
     KS_ERR_MALFORMEDPATH            = 0x1001,
     KS_ERR_NETWORKERROR             = 0x1002,
@@ -90,8 +109,9 @@ enum KS_RESULT_ENUM {
 };
 typedef enum_t KS_RESULT;
 
-/*
- * A/V stuff
+
+/* ----------------------------------------------------------------------------
+ * Authentification & verification (aka A/V) stuff.
  */
 enum KS_AUTH_TYPE_ENUM {
     KS_AUTH_NONE   = 0, /* NONE-A/V-Module   */
@@ -99,28 +119,46 @@ enum KS_AUTH_TYPE_ENUM {
 };
 typedef enum_t KS_AUTH_TYPE;
 
-/*
+
+/* ----------------------------------------------------------------------------
  * Object types (flags). When browsing through the object tree, these flags
  * can be combined (or-ed) to find any object belonging to one of the types
  * listed in the flag set.
  */
 enum KS_OBJ_TYPE_ENUM {
-    KS_OT_DOMAIN   = 0x0001, /* object is a domain   */
-    KS_OT_VARIABLE = 0x0002, /* object is a variable */
-    KS_OT_HISTORY  = 0x0004, /* object is a history  */
-    KS_OT_ANY      = KS_OT_DOMAIN | KS_OT_VARIABLE  
+    KS_OT_DOMAIN    = 0x0001, /* object is a domain    */
+    KS_OT_VARIABLE  = 0x0002, /* object is a variable  */
+    KS_OT_HISTORY   = 0x0004, /* object is a history   */
+    KS_OT_LINK      = 0x0008, /* object is a link      */
+    KS_OT_STRUCTURE = 0x0010, /* object is a structure */
+    KS_OT_ANY       = KS_OT_DOMAIN | KS_OT_VARIABLE | 
+                      KS_OT_HISTORY | KS_OT_LINK | KS_OT_STRUCTURE
 	/* restricted to the currently implemented types */
 };
 typedef enum_t KS_OBJ_TYPE;
 
+
+/* ----------------------------------------------------------------------------
+ * Access modes -- this does not only embrace the "classical" access modes like
+ * read & write, but also accessing parts, instantiability, etc...
+ */
 enum KS_ACCESS_ENUM {
-    KS_AC_NONE      = 0, /* no access at all      */
-    KS_AC_READ      = 1, /* read access           */
-    KS_AC_WRITE     = 2, /* write access          */
-    KS_AC_READWRITE = 3  /* read and write access */
+    KS_AC_NONE         = 0, /* no access at all      */
+    KS_AC_READ         = 1, /* read access           */
+    KS_AC_WRITE        = 2, /* write access          */
+    KS_AC_READWRITE    = KS_AC_READ | KS_AC_WRITE,
+
+    /* object can act as a factory */
+    KS_AC_INSTANTIABLE = 0x20000000,
+    /* object is part of another object */
+    KS_AC_PART         = 0x80000000,
 };
 typedef enum_t KS_ACCESS;
 
+
+/* ----------------------------------------------------------------------------
+ *
+ */
 enum KS_STATE_ENUM {
     KS_ST_NOTSUPPORTED = 0, /* no state available          */
     KS_ST_UNKNOWN      = 1, /* state unknown at this time  */
@@ -130,6 +168,10 @@ enum KS_STATE_ENUM {
 };
 typedef enum_t KS_STATE;
 
+
+/* ----------------------------------------------------------------------------
+ * ACPLT/KS data types / variable data types.
+ */
 enum KS_VAR_TYPE_ENUM {
     KS_VT_VOID          = 0x00,
     KS_VT_BOOL          = 0x02,
@@ -141,6 +183,7 @@ enum KS_VAR_TYPE_ENUM {
     KS_VT_TIME          = 0x31,
     KS_VT_TIME_SPAN     = 0x32,
     KS_VT_STATE         = 0x38,
+    KS_VT_STRUCT        = 0x40,
     KS_VT_BYTE_VEC      = 0x81,
     KS_VT_BOOL_VEC      = 0x82,
     KS_VT_INT_VEC       = 0x90,
@@ -150,10 +193,64 @@ enum KS_VAR_TYPE_ENUM {
     KS_VT_STRING_VEC    = 0xB0,
     KS_VT_TIME_VEC      = 0xB1,
     KS_VT_TIME_SPAN_VEC = 0xB2,
+    KS_VT_TIME_SERIES   = 0xB3,
     KS_VT_STATE_VEC     = 0xB8
 };
 typedef enum_t KS_VAR_TYPE;
 
+
+/* ----------------------------------------------------------------------------
+ *
+ */
+enum KS_HIST_TYPE_ENUM {
+    KS_HT_LOG           = 0x00,
+    KS_HT_BOOL          = 0x02,
+    KS_HT_INT           = 0x10,
+    KS_HT_UINT          = 0x11,
+    KS_HT_SINGLE        = 0x20,
+    KS_HT_DOUBLE        = 0x21,
+    KS_HT_STRING        = 0x30,
+    KS_HT_TIME          = 0x31,
+    KS_HT_TIME_SPAN     = 0x32,
+
+    KS_HT_TYPE_MASK     = 0xFFFF,
+
+    KS_HT_TIME_DRIVEN   = 0x80000000,
+    KS_HT_CHANGE_DRIVEN = 0x40000000
+};
+typedef enum_t KS_HIST_TYPE;
+
+
+/* ----------------------------------------------------------------------------
+ * Placement hint used with object management systems which support ordered
+ * sets of children/parts.
+ */
+enum KS_PLACEMENT_HINT_ENUM {
+    KS_PMH_DEFAULT = 0,
+    KS_PMH_BEGIN   = 1,
+    KS_PMH_END     = 2,
+    KS_PMH_BEFORE  = 3,
+    KS_PMH_AFTER   = 4
+};
+typedef enum_t KS_PLACEMENT_HINT;
+
+/* ----------------------------------------------------------------------------
+ *
+ */
+enum KS_INTERPOLATION_ENUM {
+    KS_IPM_NONE    = 0x0000,
+    KS_IPM_LINEAR  = 0x0001,
+    KS_IPM_MIN     = 0x0002,
+    KS_IPM_MAX     = 0x0004,
+    KS_IPM_HOLD    = 0x0008,
+    KS_IPM_DEFAULT = 0x8000
+};
+typedef enum_t KS_INTERPOLATION_MODE;
+
+
+/* ----------------------------------------------------------------------------
+ * Service codes. Dial 1-800-ACPLTKS for more information =:)
+ */
 enum KS_SVC_ENUM {
     KS_NULL          = 0,
     /*
@@ -167,15 +264,37 @@ enum KS_SVC_ENUM {
      * The GetPP services group.
      */
     KS_GETPP         = 0x00000001,
+    KS_GETEP         = 0x00000002,
     
     /*
      * The variable access service group.
      */
     KS_GETVAR        = 0x00000101,
     KS_SETVAR        = 0x00000102,
-    KS_EXGDATA       = 0x00000103
+    KS_EXGDATA       = 0x00000103,
+
+    /*
+     * Object management service group.
+     */
+    KS_CREATEOBJECT  = 0x00000201,
+    KS_DELETEOBJECT  = 0x00000202,
+    KS_RENAMEOBJECT  = 0x00000203,
+
+    KS_LINK          = 0x00000301,
+    KS_UNLINK        = 0x00000302,
 };
+
+
+/*
+ * Please note that although an objects name (identifier) can't be longer than
+ * 255 characters in length, there is no limit on the path to an object. Thus,
+ * a full path and identifier name can be of arbitrary length.
+ */
+const unsigned long KS_NAME_MAXLEN     =  255;
+const unsigned long KS_COMMENT_MAXLEN  = 4095;
+const unsigned long KS_TECHUNIT_MAXLEN =   63;
+const unsigned long KS_SIMPLEID_MAXLEN =  255;
+
     
 #endif
-
 /* End of ks/ks.h */
