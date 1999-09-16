4b8c698d7997b2fdc8d0729e26470fa38cd2bb03
harald <harald@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
1999-09-16 10:54:58 +0000

    Changed from PLT License to Artistic License.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@1265 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/plt/include/plt/debug.h
+++ history/cvs-legacy/acplt/trunk/plt/include/plt/debug.h
@@ -1,40 +1,24 @@
 /* -*-plt-c++-*- */
 #ifndef PLT_DEBUG_INCLUDED
 #define PLT_DEBUG_INCLUDED
-/* $Header: /home/david/cvs/acplt/plt/include/plt/debug.h,v 1.17 1999-09-06 08:45:59 dirk Exp $ */
+/* $Header: /home/david/cvs/acplt/plt/include/plt/debug.h,v 1.18 1999-09-16 10:54:54 harald Exp $ */
 /*
- * Copyright (c) 1996, 1997
- * Chair of Process Control Engineering,
- * Aachen University of Technology.
+ * Copyright (c) 1996, 1997, 1998, 1999
+ * Lehrstuhl fuer Prozessleittechnik, RWTH Aachen
+ * D-52064 Aachen, Germany.
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must print or display the above
- *    copyright notice either during startup or must have a means for
- *    the user to view the copyright notice.
- * 3. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the Chair of Process Control Engineering nor the
- *    name of the Aachen University of Technology may be used to endorse or
- *    promote products derived from this software without specific prior
- *    written permission.
+ * This file is part of the ACPLT/KS Package which is licensed as open
+ * source under the Artistic License; you can use, redistribute and/or
+ * modify it under the terms of that license.
  *
- * THIS SOFTWARE IS PROVIDED BY THE CHAIR OF PROCESS CONTROL ENGINEERING
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE CHAIR OF PROCESS CONTROL
- * ENGINEERING BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * You should have received a copy of the Artistic License along with
+ * this Package; see the file ARTISTIC-LICENSE. If not, write to the
+ * Copyright Holder.
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES
+ * OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  */
 /* Author: Martin Kneissl <martin@plt.rwth-aachen.de> */
 /* 
