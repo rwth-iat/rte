b2f13fcd8c723f6b95de1d01c416af4b3af43a5f
harald <harald@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
2000-04-12 07:21:52 +0000

    Bumped up the prerelease version string once again to reflect changes were made.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@1315 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/ks/include/ks/ks.h
+++ history/cvs-legacy/acplt/trunk/ks/include/ks/ks.h
@@ -1,5 +1,5 @@
 /* -*-plt-c++-*- */
-/* $Header: /home/david/cvs/acplt/ks/include/ks/ks.h,v 1.34 2000-04-11 14:13:46 harald Exp $ */
+/* $Header: /home/david/cvs/acplt/ks/include/ks/ks.h,v 1.35 2000-04-12 07:21:52 harald Exp $ */
 #ifndef KS_KS_INCLUDED
 #define KS_KS_INCLUDED
 /*
@@ -60,7 +60,7 @@ extern "C" {
  * information about with which version of the C++ communication library your
  * server was linked with.
  */
-#define KS_VERSION_STRING "1.2.0pre2"
+#define KS_VERSION_STRING "1.2.0pre3"
 
 	 
 /* ----------------------------------------------------------------------------
