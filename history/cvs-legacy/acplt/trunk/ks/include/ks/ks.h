8dac5f2dee4b0b5e763ca8df0da96614b3841147
harald <harald@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
2005-02-14 13:52:57 +0000

    bumped up the library version number
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@2032 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/ks/include/ks/ks.h
+++ history/cvs-legacy/acplt/trunk/ks/include/ks/ks.h
@@ -1,5 +1,5 @@
 /* -*-c++-*- */
-/* $Header: /home/david/cvs/acplt/ks/include/ks/ks.h,v 1.47 2004-07-20 11:46:36 harald Exp $ */
+/* $Header: /home/david/cvs/acplt/ks/include/ks/ks.h,v 1.48 2005-02-14 13:52:57 harald Exp $ */
 #ifndef KS_KS_INCLUDED
 #define KS_KS_INCLUDED
 /*
@@ -60,7 +60,7 @@ extern "C" {
  * information about with which version of the C++ communication library your
  * server was linked with.
  */
-#define KS_VERSION_STRING "1.3.3"
+#define KS_VERSION_STRING "1.3.4"
 
 	 
 /* ----------------------------------------------------------------------------
