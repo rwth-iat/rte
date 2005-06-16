9828510c9f4398a8bbeec6b3c073f0493a9b309f
harald <harald@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
2005-06-16 11:40:29 +0000

    requires include for client functionality
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@2107 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt/trunk/ks/examples/tserver.cpp
+++ history/cvs-legacy/acplt/trunk/ks/examples/tserver.cpp
@@ -1,5 +1,5 @@
 /* -*-plt-c++-*- */
-/* $Header: /home/david/cvs/acplt/ks/examples/tserver.cpp,v 1.3 1999-09-16 10:54:35 harald Exp $ */
+/* $Header: /home/david/cvs/acplt/ks/examples/tserver.cpp,v 1.4 2005-06-16 11:40:29 harald Exp $ */
 /*
  * Copyright (c) 1996, 1997, 1998, 1999
  * Lehrstuhl fuer Prozessleittechnik, RWTH Aachen
@@ -21,6 +21,7 @@
 
 /* Author: Martin Kneissl <martin@plt.rwth-aachen.de> */
 #include "ks/server.h"
+#include "ks/client.h"
 #include "ks/simpleserver.h"
 #include "ks/histdomain.h"
 
