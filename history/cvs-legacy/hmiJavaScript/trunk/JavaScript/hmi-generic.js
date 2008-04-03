43e8e799faab4c211af54f8a6427206d57d8600f
Holger Jeromin <mailgithub@katur.de>
2008-04-03 14:31:11 +0000

    Browsererkennung in generic verschoben
    gecko version:
    SVG Pfeile im Header
    valid XHTML 1.1 + MathML 2.0 + SVG 1.1 Profile
    
    explorer version:
    svg pfeile im Header
    
    
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@2643 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/hmiJavaScript/trunk/JavaScript/hmi-generic.js
+++ history/cvs-legacy/hmiJavaScript/trunk/JavaScript/hmi-generic.js
@@ -48,8 +48,8 @@
 *
 *	CVS:
 *	----
-*	$Revision: 1.4 $
-*	$Date: 2008-03-28 09:33:41 $
+*	$Revision: 1.5 $
+*	$Date: 2008-04-03 14:31:11 $
 *
 *	History:
 *	--------
@@ -96,13 +96,129 @@ function hideHeader (){
 		//menue ausschalten
 		showHeader = false;
 		document.getElementById("hmi_header").style.display = "none";
-		document.getElementById("knopftext").innerHTML = "HMI-Header einblenden";
-		document.getElementById("pfeile").innerHTML = "&#8659;";
+		document.getElementById("arrowdown1").style.visibility="visible";
+		document.getElementById("arrowdown2").style.visibility="visible";
+		document.getElementById("arrowup1").style.visibility="hidden";
+		document.getElementById("arrowup2").style.visibility="hidden";
 	} else {
 		//menue einschalten
 		showHeader = true;
 		document.getElementById("hmi_header").style.display = "block";
-		document.getElementById("knopftext").innerHTML = "HMI-Header ausblenden";
-		document.getElementById("pfeile").innerHTML = "&#8657;";
+		document.getElementById("arrowdown1").style.visibility="hidden";
+		document.getElementById("arrowdown2").style.visibility="hidden";
+		document.getElementById("arrowup1").style.visibility="visible";
+		document.getElementById("arrowup2").style.visibility="visible";
 	}
 }
+
+/*********************************
+	BrowserDetect solution provides:
+		BrowserDetect.browser
+		BrowserDetect.version
+		BrowserDetect.OS
+*********************************/
+var BrowserDetect = {
+	init: function () {
+		this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+		this.version = this.searchVersion(navigator.userAgent)
+			|| this.searchVersion(navigator.appVersion)
+			|| "an unknown version";
+		this.OS = this.searchString(this.dataOS) || "an unknown OS";
+	},
+	searchString: function (data) {
+		for (var i=0;i<data.length;i++)	{
+			var dataString = data[i].string;
+			var dataProp = data[i].prop;
+			this.versionSearchString = data[i].versionSearch || data[i].identity;
+			if (dataString) {
+				if (dataString.indexOf(data[i].subString) != -1)
+					return data[i].identity;
+			}
+			else if (dataProp)
+				return data[i].identity;
+		}
+	},
+	searchVersion: function (dataString) {
+		var index = dataString.indexOf(this.versionSearchString);
+		if (index == -1) return;
+		return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
+	},
+	dataBrowser: [
+		{ 	string: navigator.userAgent,
+			subString: "OmniWeb",
+			versionSearch: "OmniWeb/",
+			identity: "OmniWeb"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Apple",
+			identity: "Safari"
+		},
+		{
+			prop: window.opera,
+			identity: "Opera"
+		},
+		{
+			string: navigator.vendor,
+			subString: "iCab",
+			identity: "iCab"
+		},
+		{
+			string: navigator.vendor,
+			subString: "KDE",
+			identity: "Konqueror"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Firefox",
+			identity: "Firefox"
+		},
+		{
+			string: navigator.vendor,
+			subString: "Camino",
+			identity: "Camino"
+		},
+		{		// for newer Netscapes (6+)
+			string: navigator.userAgent,
+			subString: "Netscape",
+			identity: "Netscape"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "MSIE",
+			identity: "Explorer",
+			versionSearch: "MSIE"
+		},
+		{
+			string: navigator.userAgent,
+			subString: "Gecko",
+			identity: "Mozilla",
+			versionSearch: "rv"
+		},
+		{ 		// for older Netscapes (4-)
+			string: navigator.userAgent,
+			subString: "Mozilla",
+			identity: "Netscape",
+			versionSearch: "Mozilla"
+		}
+	],
+	dataOS : [
+		{
+			string: navigator.platform,
+			subString: "Win",
+			identity: "Windows"
+		},
+		{
+			string: navigator.platform,
+			subString: "Mac",
+			identity: "Mac"
+		},
+		{
+			string: navigator.platform,
+			subString: "Linux",
+			identity: "Linux"
+		}
+	]
+
+};
+BrowserDetect.init();
