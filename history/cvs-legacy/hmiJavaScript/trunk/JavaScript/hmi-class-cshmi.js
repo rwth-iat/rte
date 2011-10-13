921262a037ce55007b81e1c4efec1245ed463039
Holger Jeromin <mailgithub@katur.de>
2011-10-13 09:09:08 +0000

    keywords für cshmi hinzugefügt
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@5354 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/hmiJavaScript/trunk/JavaScript/hmi-class-cshmi.js
+++ history/cvs-legacy/hmiJavaScript/trunk/JavaScript/hmi-class-cshmi.js
@@ -47,8 +47,8 @@
 *
 *	CVS:
 *	----
-*	$Revision: 1.26 $
-*	$Date: 2011-03-11 15:20:21 $
+*	$Revision$
+*	$Date$
 *
 *	History:
 *	--------
@@ -794,7 +794,7 @@ cshmi.prototype = {
 		}
 	}
 };
-var filedate = "$Date: 2011-03-11 15:20:21 $";
+var filedate = "$Date$";
 filedate = filedate.substring(7, filedate.length-2);
 if ("undefined" == typeof HMIdate){
 	HMIdate = filedate;
