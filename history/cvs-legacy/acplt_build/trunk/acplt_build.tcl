95c9d85b56b011f7d7d1cdbc71b4ae091d2c71fc
Stasik0 <github@stasik.com>
2011-10-20 08:45:17 +0000

    lib dir will not be in the runtime release
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@5393 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/cvs-legacy/acplt_build/trunk/acplt_build.tcl
+++ history/cvs-legacy/acplt_build/trunk/acplt_build.tcl
@@ -589,11 +589,12 @@ if {$release == 1} {
     file delete -force ov_builder.exe
     file delete -force ov_codegen
     file delete -force ov_codegen.exe
-	file delete -force ov_makmak
+    file delete -force ov_makmak
     file delete -force ov_makmak.exe
     cd $releasedir
     file delete -force include
     file delete -force model
+    file delete -force lib
     cd $basedir
 	print_msg "Compressing"
     if { $os == "linux" } then {
