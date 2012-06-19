9d39fd2e9577c1324909920423279331e3c402de
lars <lars@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
2012-06-19 11:17:51 +0000

    altered state machine: doSend remains TRUE until answer is received typemethod only sends an xdr if block is in a final state (not init and not busy)
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo/develop/ks/trunk/fbcomlib@6542 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- source/setDouble.c
+++ source/setDouble.c
@@ -19,17 +19,17 @@
  * Well - it could....
  */
 OV_DLLFNCEXPORT OV_DOUBLE fbcomlib_setDouble_senddouble_get(
-    OV_INSTPTR_fbcomlib_setDouble          pobj
+		OV_INSTPTR_fbcomlib_setDouble          pobj
 ) {
-    return pobj->v_senddouble;
+	return pobj->v_senddouble;
 }
 
 /**
  * The Double, witch has to be transmitted
  */
 OV_DLLFNCEXPORT OV_RESULT fbcomlib_setDouble_senddouble_set(
-    OV_INSTPTR_fbcomlib_setDouble          pobj,
-    const OV_DOUBLE  value
+		OV_INSTPTR_fbcomlib_setDouble          pobj,
+		const OV_DOUBLE  value
 ) {
 	if(!(pobj->v_doSend)){
 		pobj->v_senddouble = value;
@@ -44,17 +44,17 @@ OV_DLLFNCEXPORT OV_RESULT fbcomlib_setDouble_senddouble_set(
  * @todo Could the GET be removed???
  */
 OV_DLLFNCEXPORT OV_BOOL fbcomlib_setDouble_doReset_get(
-	    OV_INSTPTR_fbcomlib_setDouble          pobj
+		OV_INSTPTR_fbcomlib_setDouble          pobj
 ) {
-    return pobj->v_doResetAbstract;
+	return pobj->v_doResetAbstract;
 }
 
 /**
  * If TRUE, Does the reset FBComCommon and removes senddouble additionally
  */
 OV_DLLFNCEXPORT OV_RESULT fbcomlib_setDouble_doReset_set(
-	    OV_INSTPTR_fbcomlib_setDouble          pobj,
-	    const OV_BOOL  value
+		OV_INSTPTR_fbcomlib_setDouble          pobj,
+		const OV_BOOL  value
 ) {
 	if(value){
 		fbcomlib_FBComCommon_doResetAbstract_set((OV_INSTPTR_fbcomlib_FBComCommon)pobj, value);
@@ -67,30 +67,30 @@ OV_DLLFNCEXPORT OV_RESULT fbcomlib_setDouble_doReset_set(
  * Individual startup method for creating ksapi_setdouble object for later communication
  */
 OV_DLLFNCEXPORT void fbcomlib_setDouble_startup(
-	OV_INSTPTR_ov_object 	pobj
+		OV_INSTPTR_ov_object 	pobj
 ) {
-    //~ OV_INSTPTR_fbcomlib_setDouble pinst = Ov_StaticPtrCast(fbcomlib_setDouble, pobj);
+	//~ OV_INSTPTR_fbcomlib_setDouble pinst = Ov_StaticPtrCast(fbcomlib_setDouble, pobj);
 
-    OV_INSTPTR_fbcomlib_setDouble 					setdouble = Ov_StaticPtrCast(fbcomlib_setDouble, pobj);
+	OV_INSTPTR_fbcomlib_setDouble 					setdouble = Ov_StaticPtrCast(fbcomlib_setDouble, pobj);
 	OV_VTBLPTR_fbcomlib_setDouble					stable = NULL;
 
 	OV_INSTPTR_ksapi_setDouble						ksapisetdouble = NULL;
 	OV_VTBLPTR_ksapi_setDouble						mtable = NULL;
 	OV_RESULT res = 0;
 
-    // do what the base class does first
-    fb_functionblock_startup(pobj);
+	// do what the base class does first
+	fb_functionblock_startup(pobj);
 	Ov_GetVTablePtr(fbcomlib_setDouble, stable, setdouble);
 
 	ksapisetdouble = (OV_INSTPTR_ksapi_setDouble)Ov_GetFirstChild(fbcomlib_FBComCommonAssoc, setdouble);
 	if(! ksapisetdouble){
-	  res = Ov_CreateObject(ksapi_setDouble, ksapisetdouble, setdouble, "setDouble");
-	  if (Ov_Fail(res)){
-		ov_logfile_error("Error while creating the setDouble");
-		fbcomlib_FBComCommon_state_set((OV_INSTPTR_fbcomlib_FBComCommon)setdouble, STATE_OVERROR);
-		return;
-	  }
-	  Ov_Link(fbcomlib_FBComCommonAssoc, setdouble, ksapisetdouble);
+		res = Ov_CreateObject(ksapi_setDouble, ksapisetdouble, setdouble, "setDouble");
+		if (Ov_Fail(res)){
+			ov_logfile_error("Error while creating the setDouble");
+			fbcomlib_FBComCommon_state_set((OV_INSTPTR_fbcomlib_FBComCommon)setdouble, STATE_OVERROR);
+			return;
+		}
+		Ov_Link(fbcomlib_FBComCommonAssoc, setdouble, ksapisetdouble);
 	}
 
 	Ov_GetVTablePtr(ksapi_setDouble, mtable, ksapisetdouble);
@@ -100,7 +100,7 @@ OV_DLLFNCEXPORT void fbcomlib_setDouble_startup(
 	//register the return method
 	mtable->m_registerMethod((OV_INSTPTR_ksapi_KSCommon) ksapisetdouble, stable->m_retMethod);
 	fbcomlib_FBComCommon_state_set((OV_INSTPTR_fbcomlib_FBComCommon)setdouble, STATE_OK);
-    return;
+	return;
 }
 
 
@@ -108,19 +108,21 @@ OV_DLLFNCEXPORT void fbcomlib_setDouble_startup(
  * @todo Will be removed by martin since get* Commands could use a general FBComCommon/typemethod
  */
 OV_DLLFNCEXPORT void fbcomlib_setDouble_typemethod(
-	OV_INSTPTR_fb_functionblock	pfb,
-	OV_TIME						*pltc
+		OV_INSTPTR_fb_functionblock	pfb,
+		OV_TIME						*pltc
 ) {
-    OV_INSTPTR_fbcomlib_FBComCommon 				fbcommon = Ov_StaticPtrCast(fbcomlib_FBComCommon, pfb);
-    OV_INSTPTR_fbcomlib_setDouble 					setDouble = Ov_StaticPtrCast(fbcomlib_setDouble, pfb);
+	OV_INSTPTR_fbcomlib_FBComCommon 				fbcommon = Ov_StaticPtrCast(fbcomlib_FBComCommon, pfb);
+	OV_INSTPTR_fbcomlib_setDouble 					setDouble = Ov_StaticPtrCast(fbcomlib_setDouble, pfb);
 	OV_INSTPTR_ksapi_setDouble						ksapisetdouble = NULL;
 	OV_VTBLPTR_ksapi_setDouble						mtable = NULL;
 	//OV_INSTPTR_ksapi_KSCommon						kscommon = NULL;
 	//OV_VTBLPTR_ksapi_KSCommon						mtable = NULL;
 
-	//check if read was set to true
-	if (fbcommon->v_doSend){
-		fbcommon->v_doSend = FALSE;
+	//check if something should be sent and if we are in a final state
+	if ((fbcommon->v_doSend || fbcommon->v_CyclicReactivation)
+			&& fbcommon->v_state != STATE_INIT
+			&& fbcommon->v_state != STATE_CLIENTBUSY){
+		fbcommon->v_doSend = TRUE;
 		//check if all values are set
 		if (!((fbcommon->v_server)&&(fbcommon->v_host)&&(fbcommon->v_path))){
 			ov_logfile_error("Please set all values: server,host,path");
