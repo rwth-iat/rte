4571eda0d1f1beec9f998d5d828f9b608870ba55
liyong <liyong@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
2012-08-01 13:21:14 +0000

    kopiert von legency
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@6700 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

---
+++ history/develop/ks/trunk/msgsys_example/msgreceiver/include/receiverconfig.h
@@ -0,0 +1,12 @@
+
+#include "libov/ov_macros.h"
+#include "libov/ov_path.h"
+#include "libov/ov_logfile.h"
+#include "libov/ov_string.h"
+#include "msgconfig.h"
+
+OV_DLLVAREXPORT const OV_STRING TECHROOT;
+OV_DLLVAREXPORT const OV_STRING RECEIVERROOT;
+OV_DLLVAREXPORT const OV_STRING RECEIVERCONTAINER;
+
+OV_DLLVAREXPORT const OV_TIME_SPAN RECEIVERCYCTIME;
