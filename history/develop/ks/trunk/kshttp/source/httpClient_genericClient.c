197d8ad71e5030f53baa217f8793032e8a0c33b5
Holger Jeromin <mailgithub@katur.de>
2013-08-07 15:47:43 +0000

    contentType steht nun auch zur Verfügung. Nur noch antworten mit dem Mimetype "text/*" oder "*xml*" werden in den Ausgang geschrieben.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@8153 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/develop/ks/trunk/kshttp/source/httpClient_genericClient.c
+++ history/develop/ks/trunk/kshttp/source/httpClient_genericClient.c
@@ -27,6 +27,29 @@
 void kshttp_genericHttpClient_Callback(OV_INSTPTR_ov_domain instanceCalled, OV_INSTPTR_ov_domain instanceCalling);
 
 
+OV_DLLFNCEXPORT OV_RESULT kshttp_genericHttpClient_constructor(
+	OV_INSTPTR_ov_object 	pobj
+) {
+	/*
+	*   local variables
+	*/
+	OV_INSTPTR_kshttp_genericHttpClient thisCl = Ov_StaticPtrCast(kshttp_genericHttpClient, pobj);
+	OV_RESULT    result;
+
+	/* do what the base class does first */
+	result = ksbase_ComTask_constructor(pobj);
+	if(Ov_Fail(result)){
+		return result;
+	}
+
+	ov_string_setvalue(&thisCl->v_serverPort, "80");
+	ov_string_setvalue(&thisCl->v_serverName, "<unused>");
+
+	return OV_ERR_OK;
+}
+
+
+
 OV_DLLFNCEXPORT OV_RESULT kshttp_genericHttpClient_beginCommunication_set(
 	OV_INSTPTR_kshttp_genericHttpClient          thisCl,
 	const OV_BOOL  value
@@ -39,14 +62,21 @@ OV_DLLFNCEXPORT OV_RESULT kshttp_genericHttpClient_beginCommunication_set(
 void kshttp_genericHttpClient_Callback(OV_INSTPTR_ov_domain instanceCalled, OV_INSTPTR_ov_domain instanceCalling){
 	OV_INSTPTR_kshttp_genericHttpClient	thisCl = Ov_DynamicPtrCast(kshttp_genericHttpClient, instanceCalling);
 
-	if(thisCl->v_ServerResponse.contentLength != 0){
-		thisCl->v_messageBody =  ov_database_malloc(thisCl->v_ServerResponse.contentLength+1);
-		if(!thisCl->v_messageBody){
-			kshttp_httpClientBase_reset(Ov_PtrUpCast(ksbase_ClientBase, thisCl));
-			return;
+	ov_string_setvalue(&thisCl->v_contentType, thisCl->v_ServerResponse.contentType);
+
+	if(	ov_string_match(thisCl->v_contentType, "text/*") == TRUE ||
+		ov_string_match(thisCl->v_contentType, "*xml*") == TRUE ){
+		if(thisCl->v_ServerResponse.contentLength != 0){
+			thisCl->v_messageBody =  ov_database_malloc(thisCl->v_ServerResponse.contentLength+1);
+			if(!thisCl->v_messageBody){
+				kshttp_httpClientBase_reset(Ov_PtrUpCast(ksbase_ClientBase, thisCl));
+				return;
+			}
+			memcpy(thisCl->v_messageBody, thisCl->v_ServerResponse.messageBody, thisCl->v_ServerResponse.contentLength);
+			thisCl->v_messageBody[thisCl->v_ServerResponse.contentLength] = '\0';
 		}
-		memcpy(thisCl->v_messageBody, thisCl->v_ServerResponse.messageBody, thisCl->v_ServerResponse.contentLength);
-		thisCl->v_messageBody[thisCl->v_ServerResponse.contentLength] = '\0';
+	}else{
+		ov_string_setvalue(&thisCl->v_messageBody, "binary mimetype");
 	}
 	kshttp_httpClientBase_reset(Ov_PtrUpCast(ksbase_ClientBase, thisCl));
 	return;
