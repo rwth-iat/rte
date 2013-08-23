6e3543269060072ab78d392b572029b1d1a5427c
LEvertz <l.evertz@plt.rwth-aachen.de>
2013-08-23 08:45:46 +0000

    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@8221 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

---
+++ history/develop/acplt_build/trunk/start_make.bat
@@ -0,0 +1,13 @@
+@echo off
+rem set ACPLTDEV_PATH to the AcpltDevelopment kint directory
+rem it is assumed that MinGW and tools are located in this directory
+set ACPLTDEV_PATH=%~dp0..\..\..\
+
+SET LIB=%ACPLTDEV_PATH%MinGW32\lib
+SET MINGW=%ACPLTDEV_PATH%MinGW32
+SET INCLUDE=%ACPLTDEV_PATH%MinGW32\include
+set BISON_SIMPLE=%ACPLTDEV_PATH%tools\share\bison.simple
+SET PATH=%ACPLTDEV_PATH%MinGW32\bin;%ACPLTDEV_PATH%MinGW32\lib;%ACPLTDEV_PATH%tools\bin;%PATH%
+
+make %1
+pause
