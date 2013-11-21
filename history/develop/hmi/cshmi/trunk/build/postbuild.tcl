bd3dfc98bbeec3b9300a6b56fb27e7c076fc1e37
Holger Jeromin <mailgithub@katur.de>
2013-11-21 16:14:36 +0000

    postbuild kopiert die wichtigen FBDs in ACPLT_HOME/template
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@8421 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

---
+++ history/develop/hmi/cshmi/trunk/build/postbuild.tcl
@@ -0,0 +1,23 @@
+puts "== Begin moving FBDs to template folder =="
+
+set fbdfiles {"templates/StaticDisplayComponent.fbd" "templates/Sheet_EngineeringFB.fbd" "templates/Templates_Engineering.fbd" "templates/Templates_ProcessControl.fbd"}
+
+set targetfolder "$::env(ACPLT_HOME)/templates"
+if {$targetfolder != "" && [file isdirectory $targetfolder]} {
+	foreach fbdfilefullname $fbdfiles {
+		#we are in build/nt or build/linux
+		set fbdfilefullname "../../$fbdfilefullname"
+		set fbdfilename [file tail $fbdfilefullname]
+		set targetname "$targetfolder/$fbdfilename"
+		puts -nonewline "Copying file $fbdfilefullname to $targetfolder..."
+		if {[file exists $targetname] == 1} {
+			puts -nonewline "deleting old..."
+			file delete $targetname
+		}
+		file copy $fbdfilefullname $targetname
+		puts "done."
+	}
+} else {
+	puts "Targetfolder $targetfolder not found. Aborting."
+}
+puts "== End processing templates =="
