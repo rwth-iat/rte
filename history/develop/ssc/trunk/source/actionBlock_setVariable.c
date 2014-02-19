33e0346bbcdda0cf96dcb79b680a0dbb67ff6837
Holger Jeromin <mailgithub@katur.de>
2014-02-19 15:37:55 +0000

    result variable initialisiert.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@8610 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/develop/ssc/trunk/source/actionBlock_setVariable.c
+++ history/develop/ssc/trunk/source/actionBlock_setVariable.c
@@ -56,7 +56,7 @@ OV_DLLFNCEXPORT void ssc_setVariable_typemethod(
     OV_INSTPTR_ssc_setVariable pinst = Ov_StaticPtrCast(ssc_setVariable, pfb);
     OV_INSTPTR_ssc_step  pStep= Ov_DynamicPtrCast(ssc_step, Ov_GetParent(ov_containment, pinst));
     //OV_INSTPTR_ssc_sscHeader  pSSC= Ov_DynamicPtrCast(ssc_sscHeader, Ov_GetParent(ov_containment, pStep));
-    OV_RESULT    result;
+    OV_RESULT    result = OV_ERR_OK;
     OV_UINT stringCount;
     OV_STRING* pathToVariable;
     OV_INSTPTR_ov_object pObj = NULL;
