c76e603f56994da356c4d3ae86973a92f89fda32
holger <holger@7a3915f3-eddd-4ae9-ab37-eb2ea51f0265>
2014-06-10 12:05:32 +0000

    https://www.mnot.net/blog/2014/06/07/rfc2616_is_dead "Don’t use RFC2616. Delete it from your hard drives, bookmarks, and burn (or responsibly recycle) any copies that are printed out." aka RFC7230 - RFC7235 is released
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo/develop/ks/trunk/kshttp@8803 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- source/argshandling.c
+++ source/argshandling.c
@@ -247,8 +247,8 @@ OV_RESULT kshttp_parse_http_header_from_client(KSHTTP_REQUEST *clientRequest, KS
 	ov_string_freelist(pallheaderslist);
 
 	if(ov_string_compare(clientRequest->version, "1.1") == OV_STRCMP_EQUAL && httphostsend == FALSE){
-		/* RFC 2616: "All Internet-based HTTP/1.1 servers MUST respond with a 400 (Bad Request)
-		status code to any HTTP/1.1 request message which lacks a Host header field." */
+		/* RFC 7230 Section 5.4: "A server MUST respond with a 400 (Bad Request) status
+		 * code to any HTTP/1.1 request message that lacks a Host header field..." */
 		PARSE_HTTP_HEADER_RETURN OV_ERR_BADPARAM; //400
 	}
 	//try setting format via url parameter
