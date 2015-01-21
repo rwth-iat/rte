a786f5180029175b05b64eeb7f90a220caf6b8c6
LEvertz <l.evertz@plt.rwth-aachen.de>
2015-01-21 09:48:00 +0000

    we have UDP-connection for ks now
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@9356 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

---
+++ history/develop/ks/trunk/UDPbind/include/UDPbind_config.h
@@ -0,0 +1,30 @@
+#ifndef UDPbind_confih_INCLUDED
+#define UDPbind_confih_INCLUDED
+
+#include "ksbase_helper.h"
+
+//Size of data received per chunk
+#define UDPbind_CHUNKSIZE			1460	/*	maximum size of tcp segment over ethernet IP 	*/
+
+//timeouts for destroying inactive Channels
+#define UDPbind_TTL_AFTER_ASSOC		1200
+#define UDPbind_TTL_BEFORE_ASSOC	60
+
+//timeout after receiving an incomplete package (erase of data)
+#define UDPbind_TIMEOUT_PARTIALPACKAGE	300
+
+//constates  0 - closed; 1 - opening; 2 - open; 4 - closing; 8 - sending; 16 - receiving; 32 - awaiting response; 128 - indicates error; 255 generic error
+#define UDPbind_CONNSTATE_CLOSED			KSBASE_CONNSTATE_CLOSED
+#define UDPbind_CONNSTATE_OPENING			KSBASE_CONNSTATE_OPENING
+#define UDPbind_CONNSTATE_OPEN				KSBASE_CONNSTATE_OPEN
+#define UDPbind_CONNSTATE_CLOSING			KSBASE_CONNSTATE_CLOSING
+#define UDPbind_CONNSTATE_SENDING			KSBASE_CONNSTATE_SENDING
+#define UDPbind_CONNSTATE_RECEIVING			KSBASE_CONNSTATE_RECEIVING
+#define UDPbind_CONNSTATE_AWAITINGRESPONSE	KSBASE_CONNSTATE_AWAITINGRESPONSE
+#define UDPbind_CONNSTATE_GENERICERROR		KSBASE_CONNSTATE_GENERICERROR
+#define UDPbind_CONNSTATE_ERRORINDICATOR	KSBASE_CONNSTATE_ERRORINDICATOR
+#define UDPbind_CONNSTATE_COULDNOTOPEN		KSBASE_CONNSTATE_COULDNOTOPEN
+#define UDPbind_CONNSTATE_SENDERROR			KSBASE_CONNSTATE_SENDERROR
+#define UDPbind_CONNSTATE_RECEIVEERROR		KSBASE_CONNSTATE_RECEIVEERROR
+
+#endif
---
+++ history/develop/ks/trunk/UDPbind/include/UDPbind_helper.h
@@ -0,0 +1,13 @@
+#ifndef UDPbind_helper_INCLUDED
+#define UDPbind_helper_INCLUDED
+
+#if OV_SYSTEM_NT
+ #define CLOSE_SOCKET closesocket
+ #define IOCTL_SOCKET ioctlsocket
+#else
+ #define CLOSE_SOCKET close
+ #define IOCTL_SOCKET ioctl
+#endif
+
+
+#endif
