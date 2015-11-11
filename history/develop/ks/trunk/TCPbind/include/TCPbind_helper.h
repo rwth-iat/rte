0230daac6cb923ab80ef47f01a15fcec96af7624
Holger Jeromin <mailgithub@katur.de>
2015-11-11 14:42:15 +0000

    socket defines sind in der ovt besser aufgehoben.
    git-svn-id: https://dev.plt.rwth-aachen.de/acplt-repo@9860 7a3915f3-eddd-4ae9-ab37-eb2ea51f0265

--- history/develop/ks/trunk/TCPbind/include/TCPbind_helper.h
+++ history/develop/ks/trunk/TCPbind/include/TCPbind_helper.h
@@ -1,18 +1,6 @@
 #ifndef TCPbind_helper_INCLUDED
 #define TCPbind_helper_INCLUDED
 
-#if OV_SYSTEM_NT
- #define TCPBIND_INVALID_SOCKET INVALID_SOCKET
- #define TCPBIND_SOCKET_ERROR SOCKET_ERROR
- #define TCPBIND_SOCKET SOCKET
- #define TCPBIND_CLOSE_SOCKET closesocket
-#else
- #define TCPBIND_INVALID_SOCKET (-1)
- #define TCPBIND_SOCKET_ERROR (-1)
- #define TCPBIND_SOCKET int
- #define TCPBIND_CLOSE_SOCKET close
-#endif
-
 #define OV_TCPBIND_SETSOCKET2INT(varsocket, varint)\
 	if(varsocket == TCPBIND_INVALID_SOCKET)	\
 		varint = -1;	\
