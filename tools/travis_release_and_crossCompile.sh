7b996c94caa9385222e245f56192a41826480ff8
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-08 15:49:47 +0100

    first test for windows build

--- tools/travis_release_and_crossCompile.sh
+++ tools/travis_release_and_crossCompile.sh
@@ -6,4 +6,16 @@ tar -pczf acpltRTE-linux.tar.gz acplt
 mv acpltRTE-linux.tar.gz ../`date +%F:%R:%S`_acpltRTE-linux.tar.gz
 cd ..
 
-apt-cache search mingw
+# windows build
+# copy local binaries
+mkdir localBins
+cp ./build/acplt/system/sysbin/* localBins/.
+export CPATH=/usr/i686-w64-mingw32/include/
+export PATH=~/rte/localBins/:$PATH
+echo $PATH
+cd ./build
+rm -R acplt acplt.build
+tclsh acplt_build.tcl cross windows32.cross
+zip -r acpltRTE-win32.zip acplt
+mv acpltRTE-win32.zip ../`date +%F:%R:%S`_acpltRTE-win32.zip
+cd ..
