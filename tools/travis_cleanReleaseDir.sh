5b191ae460dc8179b388c78193270fc4fc99d28b
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-08 18:34:11 +0100

    deleting all but the last ten releases

--- tools/travis_cleanReleaseDir.sh
+++ tools/travis_cleanReleaseDir.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 cd ./rte-www/releases/
-echo "Deleting empty Release archives"
-
+mkdir tmpSafe
+cp fb_dbcommands.exe tmpSafe/fb_dbcommands.exe
+echo "Deleting empty Release archives (everything smaller than 2M)"
+rm `find . -size -2000k`
+echo "Saving the 10 latest releases of everything"
+cp `find . -name '*-linux32.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp `find . -name '*-linux64.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp `find . -name '*-win32.zip' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp `find . -name '*-RPi.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+echo "Deleting everything else"
+rm *
+mv tmpSafe/* .
+rm -R tmpSafe
+echo "Cleanup done."
 cd ../../
