9eff6b09f601c30853cdb4a494e785a7c1eccf2f
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-08 17:32:05 +0100

    typo

--- tools/travis_release_64.sh
+++ tools/travis_release_64.sh
@@ -2,7 +2,7 @@
 # pack the linux release
 echo "Packing the Linux 64bit release..."
 cd ./build
-tar -pczf acpltRTE-linux.tar.gz acplt
+tar -pczf acpltRTE-linux64.tar.gz acplt
 mv acpltRTE-linux64.tar.gz ../acpltRTE-linux64.tar.gz
 cd ..
 echo "Done"
