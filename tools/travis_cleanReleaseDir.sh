19bcf7c2ca5eab7fcac861179e7d5f3a28667e05
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-08 18:59:19 +0100

    nicer cleanUp

--- tools/travis_cleanReleaseDir.sh
+++ tools/travis_cleanReleaseDir.sh
@@ -3,14 +3,14 @@ cd ./rte-www/releases/
 mkdir tmpSafe
 cp fb_dbcommands.exe tmpSafe/fb_dbcommands.exe
 echo "Deleting empty Release archives (everything smaller than 2M)"
-rm `find . -size -2000k`
+rm `find . -type f -size -2000k`
 echo "Saving the 10 latest releases of everything"
 cp `find . -name '*-linux32.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
 cp `find . -name '*-linux64.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
 cp `find . -name '*-win32.zip' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
 cp `find . -name '*-RPi.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
 echo "Deleting everything else"
-rm *
+rm `find . -type f`
 mv tmpSafe/* .
 rm -R tmpSafe
 echo "Cleanup done."
