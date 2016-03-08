2c689ef55646f9606053e39bd2dffc509a7cf837
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-08 15:09:46 +0100

    going on with travis and releases

---
+++ tools/travis_release_and_crossCompile.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+# pack the linux release
+cd ./build
+tar -pczf acpltRTE-linux.tar.gz acplt
+mv acpltRTE-linux.tar.gz ../`date +%F:%R:%S`_acpltRTE-linux.tar.gz
+cd ..
+
+apt-cache search mingw
