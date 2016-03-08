34d80cf5a2642cf052b6b49115e8256266feea05
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-08 17:28:40 +0100

    packaging of 64bit release

--- tools/travis_release_64.sh
+++ tools/travis_release_64.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-
+# pack the linux release
+echo "Packing the Linux 64bit release..."
+cd ./build
+tar -pczf acpltRTE-linux.tar.gz acplt
+mv acpltRTE-linux64.tar.gz ../acpltRTE-linux64.tar.gz
+cd ..
+echo "Done"
