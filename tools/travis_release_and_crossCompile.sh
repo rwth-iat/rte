a8da5149800628e5ba78349cdb8e7e2bd913221c
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-08 18:14:22 +0100

    added echos

--- tools/travis_release_and_crossCompile.sh
+++ tools/travis_release_and_crossCompile.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # pack the linux release
+echo "packing the 32bit Linux release"
 cd ./build
 tar -pczf acpltRTE-linux32.tar.gz acplt
 mv acpltRTE-linux32.tar.gz ../acpltRTE-linux32.tar.gz
 cd ..
+echo "Done."
 
 # windows build
 # copy local binaries
+echo "Building Windows release"
 mkdir ~/localBins
 cp ./build/acplt/system/sysbin/* ~/localBins/.
 export CPATH=/usr/i686-w64-mingw32/include/
@@ -15,12 +18,15 @@ export PATH=/home/travis/localBins:$PATH
 cd ./build
 rm -R acplt acplt.build
 tclsh acplt_build.tcl cross windows32.cross
+echo "Packing Windows release"
 zip -qr acpltRTE-win32.zip acplt
 mv acpltRTE-win32.zip ../acpltRTE-win32.zip
 cd ..
+echo "Done"
 
 # raspberry pi build
 # get toolchain
+echo "Building release for Raspberry Pi"
 mkdir ~/RaspberryTools
 cd ~/RaspberryTools
 git clone https://github.com/raspberrypi/tools
@@ -29,10 +35,10 @@ rm -R acplt acplt.build
 export CPATH=/home/travis/RaspberryTools/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/lib/gcc/arm-linux-gnueabihf/4.8.3/include/
 export PATH=/home/travis/RaspberryTools/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64/bin/:$PATH
 tclsh acplt_build.tcl cross raspberryPi.cross
-tail -n 100 acplt_build.log
+echo "Packing Release for Raspberry Pi"
 tar -pczf acpltRTE-RPi.tar.gz acplt
 mv acpltRTE-RPi.tar.gz ../acpltRTE-RPi.tar.gz
 cd ..
-
+echo "Done."
 
 
