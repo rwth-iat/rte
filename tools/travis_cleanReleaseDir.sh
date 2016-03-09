932f734755029eda2bcfacd3edf3fdf13d1d98ae
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-09 09:14:40 +0100

    preventing delete of everything murmur murmur

--- tools/travis_cleanReleaseDir.sh
+++ tools/travis_cleanReleaseDir.sh
@@ -2,14 +2,14 @@
 cd ./rte-www/releases/
 mkdir tmpSafe
 echo "Deleting empty Release archives (everything smaller than 2M)"
-rm -v `find . -name '*acpltRTE-*' -type f -size -2000k`
+rm -v `find . -maxdepth 1  -name '*acpltRTE-*' -type f -size -2000k`
 echo "Saving the 10 latest releases of everything"
-cp -v `find . -name '*acpltRTE-linux32.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
-cp -v `find . -name '*acpltRTE-linux64.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
-cp -v `find . -name '*acpltRTE-win32.zip' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
-cp -v `find . -name '*acpltRTE-RPi.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -maxdepth 1  -name '*acpltRTE-linux32.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -maxdepth 1  -name '*acpltRTE-linux64.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -maxdepth 1  -name '*acpltRTE-win32.zip' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -maxdepth 1  -name '*acpltRTE-RPi.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
 echo "Deleting everything else"
-rm -v `find . -name '*acpltRTE-*' -type f`
+rm -v `find . -maxdepth 1 -name '*acpltRTE-*' -type f`
 mv -v tmpSafe/* .
 rm -vR tmpSafe
 echo "Cleanup done."
