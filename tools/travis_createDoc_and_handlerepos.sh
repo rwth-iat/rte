8ebd2cc8af6e96a277b79e67bd5638a8b612ef78
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-09 13:20:22 +0100

    correctly unset CC for cross compiling

--- tools/travis_createDoc_and_handlerepos.sh
+++ tools/travis_createDoc_and_handlerepos.sh
@@ -3,8 +3,7 @@
 # starting in rte
 TAG="$(git rev-parse --short=10 HEAD)"
 
-export CC= 
-if [ ${OV_ARCH_BITWIDTH} == "32" ]; then bash ./tools/travis_release_and_crossCompile.sh ; fi
+if [ ${OV_ARCH_BITWIDTH} == "32" ]; then env -u CC bash ./tools/travis_release_and_crossCompile.sh ; fi
 if [ ${OV_ARCH_BITWIDTH} == "64" ]; then bash ./tools/travis_release_64.sh ; fi
 cd ..
 # now in acplt
