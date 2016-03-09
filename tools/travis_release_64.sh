56dfd1724113887df784c21742884ec51383f050
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-09 09:06:49 +0100

    removing dev folder from releases [ci skip]

--- tools/travis_release_64.sh
+++ tools/travis_release_64.sh
@@ -2,6 +2,7 @@
 # pack the linux release
 echo "Packing the Linux 64bit release..."
 cd ./build
+rm -R ./acplt/dev
 tar -pczf acpltRTE-linux64.tar.gz acplt
 mv acpltRTE-linux64.tar.gz ../acpltRTE-linux64.tar.gz
 cd ..
