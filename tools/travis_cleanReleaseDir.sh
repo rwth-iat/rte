5e00773a91dc4a8e2f411d0c787acef06a2c1141
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-09 09:01:18 +0100

    clean up improvements [ci skip]

--- tools/travis_cleanReleaseDir.sh
+++ tools/travis_cleanReleaseDir.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 cd ./rte-www/releases/
 mkdir tmpSafe
-cp fb_dbcommands.exe tmpSafe/fb_dbcommands.exe
 echo "Deleting empty Release archives (everything smaller than 2M)"
-rm `find . -type f -size -2000k`
+rm -v `find . -name '*acpltRTE-*' -type f -size -2000k`
 echo "Saving the 10 latest releases of everything"
-cp `find . -name '*-linux32.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
-cp `find . -name '*-linux64.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
-cp `find . -name '*-win32.zip' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
-cp `find . -name '*-RPi.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -name '*acpltRTE-linux32.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -name '*acpltRTE-linux64.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -name '*acpltRTE-win32.zip' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
+cp -v `find . -name '*acpltRTE-RPi.tar.gz' -type f -printf '%T@ %p\n' | sort -n | tail -10 | cut -f2- -d" " ` tmpSafe/.
 echo "Deleting everything else"
-rm `find . -type f`
-mv tmpSafe/* .
-rm -R tmpSafe
+rm -v `find . -name '*acpltRTE-*' -type f`
+mv -v tmpSafe/* .
+rm -vR tmpSafe
 echo "Cleanup done."
 cd ../../
