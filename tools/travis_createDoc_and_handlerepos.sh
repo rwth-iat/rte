9c6f5decd9e75239e729dee3f8cc5b8ca575afc7
LEvertz <l.evertz@plt.rwth-aachen.de>
2016-03-09 12:41:35 +0100

    unmidex some cds

--- tools/travis_createDoc_and_handlerepos.sh
+++ tools/travis_createDoc_and_handlerepos.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo "Creating docs"
-mkdir html
-makeinfo --html --force doc/libov.texi -o html/ --no-warn
-cd ..
-
-cd rte
+# starting in rte
 TAG="$(git rev-parse --short=10 HEAD)"
 
 if [ ${OV_ARCH_BITWIDTH} == "32" ]; then bash ./tools/travis_release_and_crossCompile.sh ; fi
 if [ ${OV_ARCH_BITWIDTH} == "64" ]; then bash ./tools/travis_release_64.sh ; fi
 cd ..
+# now in acplt
 
 echo "Moving everything to rte-www"
 git clone -q --depth=5 -b gh-pages https://$GH_TOKEN:x-oauth-basic@github.com/acplt/rte-www
@@ -24,20 +20,31 @@ if [ ${OV_ARCH_BITWIDTH} == "64" ]; then
 fi
 
 if [ ${OV_ARCH_BITWIDTH} == "32" ]; then bash ./rte/tools/travis_cleanReleaseDir.sh ; fi
-cd rte-www
-cd ./releases
+# change into releases dir
+cd ./rte-www/releases
 if [ ${OV_ARCH_BITWIDTH} == "32" ]; then bash ../../rte/tools/travis_createListPage.sh ; fi
-cd ..
+cd ../..
+# back in acplt
 
 if [ ${OV_ARCH_BITWIDTH} == "32" ]; then
+  # change to rte
+  cd ./rte
+  echo "Creating docs"
+  mkdir html
+  makeinfo --html --force doc/libov.texi -o html/ --no-warn
+  
+  # change to rte-www
+  cd ../rte-www
   git rm -r -f ./doc/current/*
   mkdir doc
   cd doc
   mkdir current
   cd ..
+  # back in rte-www
   cp -r ../rte/html/* ./doc/current/
   git add -A ./doc/current/*
 fi
+
 git add -A ./releases/*
 git config --global user.email "rte_bot@outlook.de"
 git config --global user.name "rte-bot"
