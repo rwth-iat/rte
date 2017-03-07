385e275ecca52a4820ab5c051cad2c9282d466f2
TorbenD <t.deppe@plt.rwth-aachen.de>
2017-03-07 10:56:55 +0100

    Bugfixing
    Delete instead of comment

--- build/acplt_build.tcl
+++ build/acplt_build.tcl
@@ -874,7 +874,6 @@ set system_libs {syslibs/comm/ksbase
 set addon_libs {addonlibs/hmi/cshmi 
 				addonlibs/commclient/ksapi 
 				addonlibs/commclient/fbcomlib 
-#				addonlibs/commclient/opcuafb 
 				addonlibs/functionblock/iec61131stdfb 
 				addonlibs/functionblock/vdivde3696 
 				addonlibs/functionblock/ACPLTlab003lindyn 
