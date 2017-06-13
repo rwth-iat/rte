ca28e16cfff4efa27adbe24e62f6b715f0ce3ab2
TorbenD <t.deppe@plt.rwth-aachen.de>
2017-06-13 18:29:51 +0200

    change raspi for multi use

--- addonlibs/field/raspi/source/gpioIn.c
+++ addonlibs/field/raspi/source/gpioIn.c
@@ -25,6 +25,19 @@
 #include "libov/ov_macros.h"
 #include <wiringPi.h>
 
+
+OV_DLLFNCEXPORT OV_RESULT raspi_gpioIn_pin_set(
+    OV_INSTPTR_raspi_gpioIn          pobj,
+    const OV_INT  value
+) {
+    pobj->v_pin = value;
+    OV_INSTPTR_raspi_gpioOut pinst = Ov_StaticPtrCast(raspi_gpioOut, pobj);
+    pinst->v_initialized = FALSE;
+    return OV_ERR_OK;
+}
+
+
+
 OV_DLLFNCEXPORT void raspi_gpioIn_typemethod(
 	OV_INSTPTR_fb_functionblock	pfb,
 	OV_TIME						*pltc
--- addonlibs/field/raspi/source/gpioOut.c
+++ addonlibs/field/raspi/source/gpioOut.c
@@ -26,6 +26,18 @@
 #include <wiringPi.h>
 
 
+
+OV_DLLFNCEXPORT OV_RESULT raspi_gpioOut_pin_set(
+    OV_INSTPTR_raspi_gpioOut          pobj,
+    const OV_INT  value
+) {
+    pobj->v_pin = value;
+    OV_INSTPTR_raspi_gpioOut pinst = Ov_StaticPtrCast(raspi_gpioOut, pobj);
+    pinst->v_initialized = FALSE;
+    return OV_ERR_OK;
+}
+
+
 OV_DLLFNCEXPORT void raspi_gpioOut_typemethod(
 	OV_INSTPTR_fb_functionblock	pfb,
 	OV_TIME						*pltc
