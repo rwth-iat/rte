99a9fca8ffbe960f0434971ec527544bc8857669
TorbenD <t.deppe@plt.rwth-aachen.de>
2018-01-19 09:04:53 +0100

    adding #endif

--- addonlibs/administration/lifeCycleEntryOPCUAInterface/include/ua_lifeCycleEntry_generated_encoding_binary.h
+++ addonlibs/administration/lifeCycleEntryOPCUAInterface/include/ua_lifeCycleEntry_generated_encoding_binary.h
@@ -1,9 +1,7 @@
 /* Generated from Opc.Ua.Types.bsd, identification.bsd, Custom.Opc.Ua.AssetAdministrationShell.bsd with script /home/ubuntu/opcuaParser/tools/generate_datatypes.py
  * on host ubuntu-VirtualBox by user ubuntu at 2017-09-29 03:11:03 */
-
-#ifndef UA_LIFECYCLEENTRY_GENERATED_ENCODING_HANDLING_H_
-#define UA_LIFECYCLEENTRY_GENERATED_ENCODING_HANDLING_H_
-
+#ifndef UA_LIFECYCLEENTRY_GENERATED_ENCODING_BINARY_H_
+#define UA_LIFECYCLEENTRY_GENERATED_ENCODING_BINARY_H_
 
 #ifdef UA_NO_AMALGAMATION
 #include "ua_types_encoding_binary.h"
@@ -19,3 +17,5 @@ static UA_INLINE UA_StatusCode
 UA_LifeCycleEntry_decodeBinary(const UA_ByteString *src, size_t *offset, UA_LifeCycleEntry *dst) {
     return UA_decodeBinary(src, offset, dst, &UA_LIFECYCLEENTRY[UA_LIFECYCLEENTRY_LIFECYCLEENTRY], 0, NULL);
 }
+
+#endif
