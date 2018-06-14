e576f7248206ca469585f386d31f5cc27daf6433
VLangmaier <langmaier.vitus@gmail.com>
2018-06-14 18:24:26 +0200

    deleted

--- DevKitChanges/acplt_dev.bat
+++
@@ -1,9 +0,0 @@
-SET IFBS_HOME=%~dp0clients\iFBSpro
-SET ACPLT_HOME=%~dp0acplt
-SET LIB=%~dp0MinGW32\lib
-SET MINGW=%~dp0MinGW
-SET INCLUDE=%MINGW%\include
-SET PATH=%~dp0acplt\system\addonlibs;%~dp0acplt\system\syslibs;%~dp0acplt\system\sysbin;%MINGW%\bin;%MINGW%\lib;%PATH%
-#SET PATH=%~dp0acplt\system\addonlibs;%~dp0acplt\system\syslibs;%~dp0acplt\system\sysbin;%MINGW%\bin;%MINGW%\lib;%MINGW%\libexec\gcc\i686-w64-mingw32\4.8.3;%PATH%
-SET CC=gcc
-start "" "%~dp0eclipse\eclipse.exe" -data "%~dp0acplt\dev"
