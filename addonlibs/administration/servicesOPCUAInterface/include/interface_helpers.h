3778344b2b92eba7135daafe89a509e1568d2611
Julian Grothoff <j.grothoff@plt.rwth-aachen.de>
2019-05-23 15:52:53 +0200

    Only show input or output Arguments if they are present. Added helper function to count arguments.

---
+++ addonlibs/administration/servicesOPCUAInterface/include/interface_helpers.h
@@ -0,0 +1,13 @@
+/*
+ * interface_helpers.h
+ *
+ *  Created on: 03.04.2019
+ *      Author: Julian Grothoff
+ */
+
+#ifndef INTERFACE_HELPERS_H_
+#define INTERFACE_HELPERS_H_
+
+OV_UINT countArguments(OV_INSTPTR pobj, OV_BOOL input);
+
+#endif /* INTERFACE_HELPERS_H_ */
