cba6b845c2a3481fcf1df04b68dff8666aa2a21e
Julian Grothoff <j.grothoff@plt.rwth-aachen.de>
2019-06-06 11:46:47 +0200

    Changed open62541 to include all architectures. Bugfix unload removes all nodes.

--- syslibs/comm/opcua/source/interface.c
+++ syslibs/comm/opcua/source/interface.c
@@ -26,7 +26,11 @@
 #include "opcua_ovStore.h"
 #include "opcua_helpers.h"
 
-
+//TODO maybe its better to make a list of all nodeIds and remove them afterwards
+static void removeAllNodes(void *visitorCtx, const UA_Node *node){
+	UA_NodestoreInterface* nsi = (UA_NodestoreInterface*)visitorCtx;
+	nsi->removeNode(nsi->context, &node->nodeId);
+}
 
 OV_DLLFNCEXPORT OV_RESULT opcua_interface_uri_set(
     OV_INSTPTR_opcua_interface          pobj,
@@ -82,7 +86,6 @@ OV_DLLFNCEXPORT OV_ACCESS opcua_interface_getaccess(
 }
 
 
-//TODO subscribe to changes of namespace array and update v_trafo.index and v_types.index
 OV_DLLFNCEXPORT OV_RESULT opcua_interface_load(OV_INSTPTR_opcua_interface pobj, OV_BOOL forceLoad) {
 
 	UA_StatusCode result = UA_STATUSCODE_GOOD;
@@ -91,37 +94,37 @@ OV_DLLFNCEXPORT OV_RESULT opcua_interface_load(OV_INSTPTR_opcua_interface pobj, 
 		return OV_ERR_GENERIC;
 	}
 
-	//Load all dependent interfaces first
+	// Check whether an interface with same uri is already linked
+	if(!forceLoad){
+		OV_INSTPTR_opcua_interface pInterfaceTemp = NULL;
+		Ov_ForEachChild(opcua_serverToInterfaces, server, pInterfaceTemp){
+			if(pobj != pInterfaceTemp &&
+					ov_string_compare(pobj->v_uri, pInterfaceTemp->v_uri) == OV_STRCMP_EQUAL){
+				return OV_ERR_ALREADYEXISTS;
+			}
+		}
+	}
+
+	// Load all dependent interfaces first
 	OV_INSTPTR_opcua_interface dependentInterface = NULL;
 	Ov_ForEachChild(opcua_interfaceDependency, pobj, dependentInterface){
 		opcua_interface_load(dependentInterface, FALSE);
 		//TODO error handling
 	}
 
-	size_t index = 0;
-	UA_String uri = UA_String_fromChars(pobj->v_uri);
-	if(!forceLoad){
-		if(UA_Server_getNamespaceByName(server->v_server, uri, &index) == UA_STATUSCODE_GOOD){
-			UA_String_deleteMembers(&uri);
-			return OV_ERR_ALREADYEXISTS;
-		}
-	}
-	UA_ServerConfig * config = UA_Server_getConfig(server->v_server);
-
 	// Add NamespaceUri
-	// Try to replace empty namespace otherwise add to the end
+	// Get the namespace index if already added, otherwise add to the end
 	pobj->v_index = UA_Server_addNamespace(server->v_server, pobj->v_uri);
 
-	UA_String_deleteMembers(&uri);
-
 	// Add Datatypes
+	UA_ServerConfig * config = UA_Server_getConfig(server->v_server);
 	if(pobj->v_dataTypes){
 		pobj->v_dataTypes->next = config->customDataTypes;
 		config->customDataTypes = pobj->v_dataTypes;
 	}
 
 	UA_Nodestore_Switch *nsSwitch = UA_Server_getNodestore(server->v_server);
-	// Link the nodestore (transformation) to the namespace in the switch
+	// Link the nodestore to the namespace in the switch
 	if(pobj->v_store){
 		result = UA_Nodestore_Switch_setNodestore(nsSwitch, pobj->v_index, pobj->v_store);
 		if (result != UA_STATUSCODE_GOOD){
@@ -174,28 +177,27 @@ OV_DLLFNCEXPORT OV_RESULT opcua_interface_unload(OV_INSTPTR_opcua_interface pobj
 		}
 	}
 
-	UA_Nodestore_Switch *nsSwitch = UA_Server_getNodestore(server->v_server);
-	// Unlink the nodestore from the namespace
-	UA_Nodestore_Switch_changeNodestore(nsSwitch, pobj->v_store, NULL);
-
-
-	pobj->v_store->iterate(pobj->v_store->context, (UA_NodestoreVisitor)pobj->v_store->removeNode, pobj->v_store->context);
+	// Unlink the store and delete nodes from it
+	if(pobj->v_store != NULL){
+		UA_Nodestore_Switch *nsSwitch = UA_Server_getNodestore(server->v_server);
+		// Unlink the nodestore from the namespace
+		UA_Nodestore_Switch_changeNodestore(nsSwitch, pobj->v_store, NULL);
+		// Remove all nodes from nodestore
+		pobj->v_store->iterate(pobj->v_store->context, (UA_NodestoreVisitor)removeAllNodes, pobj->v_store);
+	}
 
 	return OV_ERR_OK;
 }
 
 
 OV_DLLFNCEXPORT OV_BOOL opcua_interface_checkNode(OV_INSTPTR_opcua_interface pobj, OV_INSTPTR_ov_object pNode, OV_STRING virtualNodePath, void *context) {
-
-    return TRUE;
+    return FALSE;
 }
 
 OV_DLLFNCEXPORT OV_BOOL opcua_interface_checkReference(OV_INSTPTR_opcua_interface pobj, OV_UINT applicationIndex, OV_INSTPTR_ov_object pNode, UA_AddReferencesItem * parentRef) {
-
     return FALSE;
 }
 
 OV_DLLFNCEXPORT UA_StatusCode opcua_interface_nodeset(UA_Server* server) {
-
     return (UA_StatusCode)0;
 }
