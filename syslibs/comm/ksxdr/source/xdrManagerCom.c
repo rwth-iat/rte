3b2aceebf4590668c1470da1fd3650dcc2b67a3f
Lars Nt <l.nothdurft@plt.rwth-aachen.de>
2019-06-12 17:56:17 +0200

    memleak fix for ksxdr
    freeing OwnPort variable of xdrmanager on shutdown

--- syslibs/comm/ksxdr/source/xdrManagerCom.c
+++ syslibs/comm/ksxdr/source/xdrManagerCom.c
@@ -267,7 +267,6 @@ OV_DLLFNCEXPORT void ksxdr_xdrManagerCom_startup(
     pinst->v_cycInterval = 5000; /*	cycle every 5 seconds at first	*/
     pinst->v_actimode = 1;
     pinst->v_Tries = 0;
-    pinst->v_OwnPort = 0;
     pinst->v_PmReqSent = FALSE;
     if(Ov_Fail(ksxdr_xdrManagerCom_getOwnPort(pinst))){
     	return;
@@ -441,6 +440,7 @@ OV_DLLFNCEXPORT void ksxdr_xdrManagerCom_shutdown(
 			ksxdr_xdrClient_requestUnRegister(Ov_StaticPtrCast(ksbase_ClientBase, xdrClient), NULL, servername.value.valueunion.val_string, 2, NULL, NULL);
 
 	}
+	ov_string_setvalue(&thisMngCom->v_OwnPort, NULL);
 	thisMngCom->v_RegisterState = XDR_MNGRCOM_REGISTERSTATE_NOTREGISTERED;	/*	set state to unregistered	*/
 	thisMngCom->v_cycInterval = 5000;	/*	5 seconds cyctime for next start up	*/
 	thisMngCom->v_Tries = 0;
