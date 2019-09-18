56fe3976005499d2ce058d71cb9d22280221876d
Michael Thies <m.thies@plt.rwth-aachen.de>
2019-09-18 12:56:30 +0200

    Delete legacy code, libraries to be moved to own project, rework structure

--- Demos/Demo_DiscoveryServer/README.md
+++
@@ -1,2 +0,0 @@
-# i40Discovery
-This demo shows the actual state of the implementation for a asset administration shell discovery server.
--- addonlibs/comm/kshttpMsgExt/build/postbuild.tcl
+++
@@ -1,3 +0,0 @@
-puts -nonewline "Removing generated static files include..."
-file delete "../../source/generated_static_index.c" -force
-puts "done"
