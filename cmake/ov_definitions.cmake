cec02e03a3d51ea49071663378f2803afd0dab7a
Michael Thies <m.thies@plt.rwth-aachen.de>
2019-10-22 10:35:58 +0200

    cmake: Abort build an MacOS (we don't support that yet)

--- cmake/ov_definitions.cmake
+++ cmake/ov_definitions.cmake
@@ -1,6 +1,8 @@
 # General OV preprocessor definitions for OV and all libs
 if (WIN32)
     add_definitions(-DOV_SYSTEM_NT=1 -DPLT_SYSTEM_NT=1 -DFD_SETSIZE=128 -DWIN32)
+elseif(APPLE)
+    message(FATAL_ERROR "Building ACPLT/OV on Mac OS is not supported.")
 else()
     add_definitions(-DOV_SYSTEM_LINUX=1)
 endif()
